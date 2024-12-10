857a9781fcecd0872126dc03787ab732f9c42288
Ghost-jpg-exe <kupreevegor9@gmail.com>
2024-12-10 20:36:02 +1000

    создана база

---
+++ kalk.py
@@ -0,0 +1,9 @@
+a=int(input('введите первое число '))
+b=int(input('введите второе число'))
+c=input('введите знак')
+if c=='+':
+    print(a+b)
+elif c=='-':
+    print(a-b)
+elif c=='*':
+    print(a*b)
