1a52f01014319e50e84cef5251cedcac1fb1c8c8
Ghost-jpg-exe <kupreevegor9@gmail.com>
2024-12-12 16:42:49 +1000

    создана первая команда

--- tg.py
+++ tg.py
@@ -1,7 +1,7 @@
 import os
 import telebot as t
-import time
 from datetime import datetime
+import time
 from telebot.types import Message as m
 dd=os.path.join(os.path.dirname(__file__))
 bot = t.telebot.TeleBot(token='7992565364:AAE1dXHxjYWgr6wbUwW7DPP3WcPVloBC4g8')
